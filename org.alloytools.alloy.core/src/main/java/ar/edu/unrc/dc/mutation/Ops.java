454b25d3e9045127ac8c1232e27fa8fb4406f5f9
Simon Emmanuel Gutierrez Brida <simon.gutierrez.brida@gmail.com>
2019-12-02 19:10:13 -0300

    Changed PRV to JER (Join Expression Replacement), implementation is not yet complete

--- org.alloytools.alloy.core/src/main/java/ar/edu/unrc/dc/mutation/Ops.java
+++ org.alloytools.alloy.core/src/main/java/ar/edu/unrc/dc/mutation/Ops.java
@@ -67,7 +67,7 @@ public enum Ops {
                      }
 
                  },
-                 PRV {
+                 JER {
 
                      @Override
                      public boolean isImplemented() {
